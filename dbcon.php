b166f256991d1f4c2227da049dd21a3349f694aa
chicknkareeenn <166093469+chicknkareeenn@users.noreply.github.com>
2024-11-14 14:15:27 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,16 +1,13 @@
 <?php  
-
 $sname = "dpg-csonachu0jms738mmhng-a.oregon-postgres.render.com";  // Full hostname of the PostgreSQL server
 $uname = "reporting_ia98_user";         // Username
 $password = "C1S8UVRh7jFTCjOkAuuV4qoZXgPfPIGG";  // Password
 $db_name = "reporting_ia98";            // Database name
-
 // Connection string for PostgreSQL
 $conn = pg_connect("host=$sname dbname=$db_name user=$uname password=$password");
 
 if (!$conn) {
-    echo "Connection Failed!";
-    exit();
+    exit('Connection failed!');
 } 
 ?>
 
