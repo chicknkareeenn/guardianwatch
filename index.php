4ea5c53801abb3f907317fc19561f7e9dc62b60e
chicknkareeenn <166093469+chicknkareeenn@users.noreply.github.com>
2024-11-14 17:15:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Crime Reports web application</title>
+  <title>Guardian Watch</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -38,13 +38,7 @@
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: Nov 17 2023 with Bootstrap v5.3.2
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+  
 </head>
 
 <body>
